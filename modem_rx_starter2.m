33ce667db12eca537dcfa80a8709d3c2452b641b
MarcEftimie <marc.eftimie@gmail.com>
2023-05-03 10:28:07 -0700

    refactored sampling

--- modem_rx_starter2.m
+++ modem_rx_starter2.m
@@ -11,7 +11,7 @@ start_idx = find_start_of_signal(y_r,x_sync);
 % we are interested in
 y_t = y_r(start_idx+length(x_sync):end); % y_t is the signal which starts at the beginning of the transmission
 
-figure(2)
+figure(1)
 y=fft(y_t);
 n = length(y_t);
 fshift = (-n/2:n/2-1)*(Fs/n);
@@ -20,7 +20,7 @@ plot(fshift,abs(yshift))
 xlabel('Frequency (Hz)')
 ylabel('Magnitude')
 y_c=y_t.*cos(2*pi*f_c/Fs*[0:length(y_t)-1]');
-figure(3)
+figure(2)
 y=fft(y_c);
 n = length(y_c);
 fshift = (-n/2:n/2-1)*(Fs/n);
@@ -29,7 +29,7 @@ plot(fshift,abs(yshift))
 xlabel('Frequency (Hz)')
 ylabel('Magnitude')
 y1 =lowpass(y_c,0.0001,Fs);
-figure(5)
+figure(3)
 y=fft(y1);
 n = length(y);
 fshift = (-n/2:n/2-1)*(Fs/n);
@@ -37,21 +37,27 @@ yshift = fftshift(y);
 plot(fshift,abs(yshift))
 xlabel('Frequency (Hz)')
 ylabel('Magnitude')
-figure(6)
+figure(4)
 clf
 plot(y1)
 title("y1")
 
-samples = [50:100:98350];
-x_d = zeros(1, length(samples));
-x_d_count = 1;
-for i = 1:length(samples)
-    if (y1(samples(1, i)) > 0)
+
+x_d = [];
+sample = 50;
+i = 1;
+while true
+    if (y1(sample) < 0.005) && (y1(sample) > -0.005)
+        break;
+    elseif (y1(sample) > 0)
         x_d(i) = 1;
     else
         x_d(i) = 0;
     end
+    sample = sample + 100;
+    i = i + 1;
 end
+x_d = x_d(1:end-1)
 
 % convert to a string assuming that x_d is a vector of 1s and 0s
 % representing the decoded bits
